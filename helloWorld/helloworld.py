b42bf0d82adce642b9e14f2f9106885b837c9673
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-07-02 11:03:18 +0200

    com2

--- helloWorld/helloworld.py
+++ helloWorld/helloworld.py
@@ -1,3 +1,7 @@
-print "hello world"
-print "hello world"
-print "hello world"
+for i in range(0,10):
+  print "hello wolrd"
+  print i
+if i == 10:
+  print "second param is the end value"
+else:
+  print "second param - 1 is the last value"
